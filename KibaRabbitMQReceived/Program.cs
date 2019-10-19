ef5c03272ad442e070aea6717ff94453abd895ba
kiba518 <liujin_hzzh@126.com>
2019-10-19 16:59:16 +0800

    KibaRabbitMQ

---
+++ KibaRabbitMQReceived/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KibaRabbitMQReceived
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            factory.UserName = "guest";
+            factory.Password = "guest";
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("kibaQueue", false, false, false, null);
+
+                    /* 这里定义了一个消费者，用于消费服务器接受的消息
+                     * C#开发需要注意下这里，在一些非面向对象和面向对象比较差的语言中，是非常重视这种设计模式的。
+                     * 比如RabbitMQ使用了生产者与消费者模式，然后很多相关的使用文章都在拿这个生产者和消费者来表述。
+                     * 但是，在C#里，生产者与消费者对我们而言，根本算不上一种设计模式，他就是一种最基础的代码编写规则。
+                     * 所以，大家不要复杂的名词吓到，其实，并没那么复杂。
+                     * 这里，其实就是定义一个EventingBasicConsumer类型的对象，然后该对象有个Received事件，该事件会在服务接收到数据时触发。
+                     */ 
+                    var consumer = new EventingBasicConsumer(channel);//消费者 
+                    channel.BasicConsume("kibaQueue", true, consumer);//消费消息 autoAck参数为消费后是否删除
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received： {0}", message);
+                    };
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ KibaRabbitMQSend/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KibaRabbitMQSend
+{
+    //rabbitmqctl stop_app 停止服务，会清除queue
+    //rabbitmqctl start_app 开启服务
+    //rabbitmqctl list_queues 查询当前队列
+    //rabbitmqctl purge_queue kibaQueue 清空指定queue队列的数据
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";//主机名，Rabbit会拿这个IP生成一个endpoint，这个很熟悉吧，就是socket绑定的那个终结点。
+            factory.UserName = "guest";//默认用户名,用户可以在服务端自定义创建，有相关命令行
+            factory.Password = "guest";//默认密码
+
+            using (var connection = factory.CreateConnection())//连接服务器，即正在创建终结点。
+            {
+                //创建一个通道，这个就是Rabbit自己定义的规则了，如果自己写消息队列，这个就可以开脑洞设计了
+                //这里Rabbit的玩法就是一个通道channel下包含多个队列Queue
+                using (var channel = connection.CreateModel())
+                {
+                    for (int i = 0; i < 100; i++)
+                    {
+                        channel.QueueDeclare("kibaQueue", false, false, false, null);//创建一个名称为kibaQueue的消息队列
+                        var properties = channel.CreateBasicProperties();
+                        properties.DeliveryMode = 1;
+                        string message = "I am Kiba518"; //传递的消息内容 
+                        channel.BasicPublish("", "kibaQueue", properties, Encoding.UTF8.GetBytes(message)); //生产消息
+                        Console.WriteLine($"Send:{message}");
+                        Thread.Sleep(3000);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
